cac252fc88179c23660b339b23af5623c2d25812
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-07-20 13:05:12 -0600

    Sabado 07

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+// IMPORTAR LOS COMPONENTES
+import Header from './layouts/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/Header.tsx
@@ -0,0 +1,25 @@
+// IMPORTS
+import React from 'react'
+
+// DECLARATION
+const Header = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Clientes</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Ventas</a></li>
+                <li><a href="#">Compras</a></li>
+                <li><a href="#">Ayuda</a></li>
+                <li><a href="#">FAQ</a></li>
+                
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+// EXPORT
+export default Header
